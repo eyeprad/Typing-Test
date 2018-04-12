adc8cf3c6113be89035b5acf39c3b36e371449e6
Glitch (comp484-project-ttt) <none>
2018-04-12 21:54:55 +0000

    🌂🚜 Checkpoint ./index.html:664185/2367 ./script.js:664185/743 ./style.css:664185/2557

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+const testWrapper = document.querySelector(".test-wrapper");
+const testArea = document.querySelector("#test-area");
+const originText = document.querySelector("#origin-text p").innerHTML;
+const resetButton = document.querySelector("#reset");
+const theTimer = document.querySelector(".timer");
 
-// prints "hi" in the browser's dev tools console
-console.log('hi');
+
+// Add leading zero to numbers 9 or below (purely for aesthetics):
+
+
+// Run a standard minute/second/hundredths timer:
+
+
+// Match the text entered with the provided text on the page:
+
+
+// Start the timer:
+
+
+// Reset everything:
+
+
+// Event listeners for keyboard input and the reset button:
